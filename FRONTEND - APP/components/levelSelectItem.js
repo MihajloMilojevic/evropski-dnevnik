b840abd9b3dabd77559e025f23df3faf7585963a
analukovic <alukovic2004@gmail.com>
2022-02-25 18:21:48 +0100

    dizajn levela ispravka

--- FRONTEND - APP/components/levelSelectItem.js
+++ FRONTEND - APP/components/levelSelectItem.js
@@ -33,7 +33,7 @@ export default function LevelSelectItem({item, navigation}) {
 const styles = StyleSheet.create({
 	container: {
 	  flex: 1,
-	  background: rgb(205,205,205),
+	  background:' rgb(205,205,205)',
       background:' radial-gradient(circle, rgba(205,205,205,1) 7%, rgba(3,22,121,1) 93%)',
 	//  backgroundColor: '#fff',
 	  alignItems: 'center',
