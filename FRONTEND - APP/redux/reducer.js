903eecc1a07885ff0cb38f4aba7ee5a8cca1664f
MihajloESTS <milojevicm374@gmail.com>
2022-02-28 03:06:46 +0100

    done

--- FRONTEND - APP/redux/reducer.js
+++ FRONTEND - APP/redux/reducer.js
@@ -10,7 +10,7 @@ const removeUser = () => {
 }
 
 const initialState = {
-	host: "https://evropski-dnevnik-dev.herokuapp.com",
+	host: "https://euroquiz.herokuapp.com",
 	user: {
 		username: "",
 		_id: "",
